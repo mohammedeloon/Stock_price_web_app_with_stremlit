b7053749a60fb4e51ce0fd6d27264e7dae52d052
Mohammed Shurrab <mohmmedshurrab07@gmail.com>
2023-05-01 23:58:17 +0200

    Edited the script to dynamically update data andd removed useless columns

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Stock_price_web_app_with_streamlit
+## Description
+A simple web application demonstrating the price(closing price and volume) of google stocks over the last 10 years
+
+## Libraries
+* yfinance: a Python library that allows you to download financial data from Yahoo Finance.
+* streamlit: a Python library that allows you to create interactive data-driven  web applications.
+* datetime: a Python module that provides classes for working with dates and times.
+
+## Images of the website:
+![image](images/steamlit1.png)
+![image](images/streamlitt2.png)
+![image](images/streamlit4.png)
--- stock_price.py
+++ stock_price.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import streamlit as st
-import pandas as pd
+from datetime import datetime
 
 st.write("""
 #  Stock Price App
@@ -11,10 +11,10 @@ shown are the stocks ***closing price*** and volume of Google:
 
 tickerSymbols = 'GOOGL'
 tickerData = yf.Ticker(tickerSymbols)
-tickerDf = tickerData.history(period='1d' , start='2010-5-31' , end='2023-3-21')
+tickerDf = tickerData.history(period='1d' , start='2010-1-1', end=datetime.today().strftime('%Y-%m-%d'))
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
-st.table(tickerDf.head(10))
+st.table(tickerDf.drop(['Dividends' , 'Stock Splits'], axis=1).copy().head(10))
 
 
 
